38b77f1a1abc935ef8f626d459eb720f486274a7
James Garden <jamesegarden@outlook.com>
2022-10-09 02:39:18 +0100

    renamed fxml file

--- src/main/java/uk/epsilondynamics/breaktime/BreakTimeApplication.java
+++ src/main/java/uk/epsilondynamics/breaktime/BreakTimeApplication.java
@@ -14,7 +14,7 @@ import javafx.util.Duration;
 public class BreakTimeApplication extends Application {
   @Override
   public void start(Stage stage) throws IOException {
-    FXMLLoader fxmlLoader = new FXMLLoader(BreakTimeApplication.class.getResource("hello-view.fxml"));
+    FXMLLoader fxmlLoader = new FXMLLoader(BreakTimeApplication.class.getResource("break-time.fxml"));
     Scene scene = new Scene(fxmlLoader.load(), 300, 200);
     stage.setTitle("BreakTime");
     stage.setScene(scene);
